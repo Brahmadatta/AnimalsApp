7fb17f18de0ea4e44c286219dda7481754d27b5b
brahmadatta <g.brahmadatta@gmail.com>
2019-10-31 23:22:03 +0530

    added fragments and action buttons

---
+++ app/src/main/java/com/example/animalsapp/view/DetailFragment.java
@@ -0,0 +1,56 @@
+package com.example.animalsapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.animalsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends Fragment {
+
+    @BindView(R.id.detailFAB)
+    FloatingActionButton detailFab;
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        ButterKnife.bind(this,view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        detailFab.setOnClickListener(view1 -> {
+
+            NavDirections action = DetailFragmentDirections.actionGoToList();
+            Navigation.findNavController(view1).navigate(action);
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/animalsapp/view/ListFragment.java
@@ -0,0 +1,58 @@
+package com.example.animalsapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.animalsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+    @BindView(R.id.listFAB)
+    FloatingActionButton listFab;
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        ButterKnife.bind(this,view);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listFab.setOnClickListener(view1 -> {
+
+            NavDirections action = ListFragmentDirections.actionGoToDetails();
+            Navigation.findNavController(view1).navigate(action);
+
+        });
+    }
+}
