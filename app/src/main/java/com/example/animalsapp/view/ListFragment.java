25dde4c8ef19b8bcc28614ebcbc206bd4dbffebb
brahmadatta <g.brahmadatta@gmail.com>
2019-11-03 22:36:25 +0530

    added swiperefresh layout and implemented viewmodel method

--- app/src/main/java/com/example/animalsapp/view/ListFragment.java
+++ app/src/main/java/com/example/animalsapp/view/ListFragment.java
@@ -6,15 +6,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.animalsapp.R;
+import com.example.animalsapp.model.AnimalModel;
+import com.example.animalsapp.viewmodel.ListViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +36,21 @@ import butterknife.ButterKnife;
 public class ListFragment extends Fragment {
 
 
+    @BindView(R.id.animalRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @BindView(R.id.listError)
+    TextView listEror;
+
+    @BindView(R.id.animalList)
+    RecyclerView animalList;
+
+    @BindView(R.id.loadingView)
+    ProgressBar loadingView;
+
+    private ListViewModel mListViewModel;
+
+    private AnimalListAdapter mListAdapter = new AnimalListAdapter();
 
     public ListFragment() {
         // Required empty public constructor
@@ -43,4 +69,46 @@ public class ListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mListViewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+
+        mListViewModel.animals.observe(getViewLifecycleOwner(), list -> {
+
+            if (list != null){
+                animalList.setVisibility(View.VISIBLE);
+                mListAdapter.updateList(list);
+
+            }
+        });
+
+        mListViewModel.loading.observe(this,loading -> {
+            loadingView.setVisibility(loading ? View.VISIBLE : View.GONE);
+            if (loading){
+                listEror.setVisibility(View.GONE);
+                animalList.setVisibility(View.GONE);
+            }
+        });
+
+        mListViewModel.loadError.observe(this,error -> {
+            listEror.setVisibility(error ? View.VISIBLE : View.GONE);
+        });
+
+        mListViewModel.refresh();
+
+        if (animalList != null){
+            animalList.setLayoutManager(new GridLayoutManager(getContext(),2));
+            animalList.setAdapter(mListAdapter);
+
+        }
+
+        mSwipeRefreshLayout.setOnRefreshListener(() -> {
+            listEror.setVisibility(View.GONE);
+            loadingView.setVisibility(View.GONE);
+            animalList.setVisibility(View.GONE);
+            mListViewModel.refresh();
+            mSwipeRefreshLayout.setRefreshing(false);
+        });
+    }
 }
--- app/src/main/java/com/example/animalsapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/animalsapp/viewmodel/ListViewModel.java
@@ -20,11 +20,29 @@ public class ListViewModel extends ViewModel {
         AnimalModel animal1 = new AnimalModel("lion");
         AnimalModel animal2 = new AnimalModel("zebra");
         AnimalModel animal3 = new AnimalModel("cheetah");
+        AnimalModel animal4 = new AnimalModel("lion");
+        AnimalModel animal5 = new AnimalModel("zebra");
+        AnimalModel animal6 = new AnimalModel("cheetah");
+        AnimalModel animal7 = new AnimalModel("lion");
+        AnimalModel animal8 = new AnimalModel("zebra");
+        AnimalModel animal9 = new AnimalModel("cheetah");
+        AnimalModel animal10 = new AnimalModel("lion");
+        AnimalModel animal11 = new AnimalModel("zebra");
+        AnimalModel animal12 = new AnimalModel("cheetah");
 
         List<AnimalModel> animalModelList = new ArrayList<>();
         animalModelList.add(animal1);
         animalModelList.add(animal2);
         animalModelList.add(animal3);
+        animalModelList.add(animal4);
+        animalModelList.add(animal5);
+        animalModelList.add(animal6);
+        animalModelList.add(animal7);
+        animalModelList.add(animal8);
+        animalModelList.add(animal9);
+        animalModelList.add(animal10);
+        animalModelList.add(animal11);
+        animalModelList.add(animal12);
 
         animals.setValue(animalModelList);
         loadError.setValue(false);
