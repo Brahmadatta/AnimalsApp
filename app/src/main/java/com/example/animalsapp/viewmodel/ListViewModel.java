d121a6fb6dd2099804830f599b08beccb7004c26
brahmadatta <g.brahmadatta@gmail.com>
2019-11-05 23:34:02 +0530

    retrieving data from retrofit

--- app/src/main/java/com/example/animalsapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/animalsapp/viewmodel/ListViewModel.java
@@ -3,50 +3,94 @@ package com.example.animalsapp.viewmodel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.animalsapp.model.AnimalApiService;
 import com.example.animalsapp.model.AnimalModel;
+import com.example.animalsapp.model.ApiKeyModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class ListViewModel extends ViewModel {
 
+    private AnimalApiService mAnimalApiService = new AnimalApiService();
+
+    private CompositeDisposable mDisposable = new CompositeDisposable();
 
     public MutableLiveData<List<AnimalModel>> animals = new MutableLiveData<List<AnimalModel>>();
     public MutableLiveData<Boolean> loadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
 
 
-    public void refresh(){
-        AnimalModel animal1 = new AnimalModel("lion");
-        AnimalModel animal2 = new AnimalModel("zebra");
-        AnimalModel animal3 = new AnimalModel("cheetah");
-        AnimalModel animal4 = new AnimalModel("lion");
-        AnimalModel animal5 = new AnimalModel("zebra");
-        AnimalModel animal6 = new AnimalModel("cheetah");
-        AnimalModel animal7 = new AnimalModel("lion");
-        AnimalModel animal8 = new AnimalModel("zebra");
-        AnimalModel animal9 = new AnimalModel("cheetah");
-        AnimalModel animal10 = new AnimalModel("lion");
-        AnimalModel animal11 = new AnimalModel("zebra");
-        AnimalModel animal12 = new AnimalModel("cheetah");
+    public void refresh() {
+        loading.setValue(true);
+        getKey();
+    }
 
-        List<AnimalModel> animalModelList = new ArrayList<>();
-        animalModelList.add(animal1);
-        animalModelList.add(animal2);
-        animalModelList.add(animal3);
-        animalModelList.add(animal4);
-        animalModelList.add(animal5);
-        animalModelList.add(animal6);
-        animalModelList.add(animal7);
-        animalModelList.add(animal8);
-        animalModelList.add(animal9);
-        animalModelList.add(animal10);
-        animalModelList.add(animal11);
-        animalModelList.add(animal12);
+    private void getKey() {
 
-        animals.setValue(animalModelList);
-        loadError.setValue(false);
-        loading.setValue(false);
+        mDisposable.add(
 
+                mAnimalApiService.getApiKey()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<ApiKeyModel>() {
+                            @Override
+                            public void onSuccess(ApiKeyModel key) {
+
+                                if (key.key.isEmpty()){
+                                    loadError.setValue(true);
+                                    loading.setValue(false);
+                                }else {
+                                    getAnimals(key.key);
+                                }
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                                e.printStackTrace();
+                                loading.setValue(false);
+                                loadError.setValue(true);
+                            }
+                        })
+        );
+
+    }
+
+    private void getAnimals(String key) {
+
+        mDisposable.add(
+                mAnimalApiService.getAnimals(key)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<AnimalModel>>() {
+                    @Override
+                    public void onSuccess(List<AnimalModel> animalModels) {
+
+                        loadError.setValue(false);
+                        animals.setValue(animalModels);
+                        loading.setValue(false);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        loading.setValue(false);
+                        loadError.setValue(true);
+                    }
+                })
+
+        );
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        mDisposable.clear();
     }
 }
