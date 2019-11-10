bdd76aff3524c18a4a1133c120e11d7cfde15543
brahmadatta <g.brahmadatta@gmail.com>
2019-11-10 22:39:12 +0530

    creating the test rule

---
+++ app/src/test/java/com/example/animalsapp/ListViewModelTest.java
@@ -0,0 +1,35 @@
+package com.example.animalsapp;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+
+import java.util.concurrent.Executor;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+public class ListViewModelTest {
+
+    @Rule
+    public InstantTaskExecutorRule mRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void setupRxSchedulers(){
+
+        Scheduler immediate = new Scheduler() {
+            @Override
+            public Worker createWorker() {
+                return new ExecutorScheduler.ExecutorWorker(runnable -> {
+                    runnable.run();
+                },true);
+            }
+        };
+
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(schedulerCallable -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(schedulerCallable -> immediate);
+    }
+}
